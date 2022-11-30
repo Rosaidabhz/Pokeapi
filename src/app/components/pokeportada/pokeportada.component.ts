444e2393579cb78f12fe8da72955c370c791e751
Rosaida <rosaidabhz@gmail.com>
2022-11-30 14:49:15 -0500

    Primer commit

---
+++ src/app/components/pokeportada/pokeportada.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokeportada',
+  templateUrl: './pokeportada.component.html',
+  styleUrls: ['./pokeportada.component.scss']
+})
+export class PokeportadaComponent {
+
+}
